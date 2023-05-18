3a26c73259b9317502f5af30c65507ea1d29142e
x <>
2023-05-18 14:51:07 +0100

    add more cities, and hardcode geolocations for some cities to avoid geolocation api rate limiting

--- lib/custom_components/preferences.dart
+++ lib/custom_components/preferences.dart
@@ -9,17 +9,16 @@ class Preferences {
   bool isLocationSetAutomatically;
   String selectedLocation;
 
-  Preferences({
-    required this.isCelsius, 
-    required this.minTemp, 
-    required this.maxTemp, 
-    required this.workInRain, 
-    required this.workInSnow, 
-    required this.workInWind, 
-    required this.workAtNight,
-    required this.isLocationSetAutomatically,
-    required this.selectedLocation
-  });
+  Preferences(
+      {required this.isCelsius,
+      required this.minTemp,
+      required this.maxTemp,
+      required this.workInRain,
+      required this.workInSnow,
+      required this.workInWind,
+      required this.workAtNight,
+      required this.isLocationSetAutomatically,
+      required this.selectedLocation});
 
   Preferences.copy(Preferences p)
       : isCelsius = p.isCelsius,
@@ -33,15 +32,32 @@ class Preferences {
         selectedLocation = p.selectedLocation;
 
   Preferences.defaultPreferences()
-      : isCelsius = true, 
-        minTemp = 18.0, 
+      : isCelsius = true,
+        minTemp = 18.0,
         maxTemp = 25.0,
-        workInRain = false, 
-        workInSnow = false, 
-        workInWind = false, 
+        workInRain = false,
+        workInSnow = false,
+        workInWind = false,
         workAtNight = false,
         isLocationSetAutomatically = true,
         selectedLocation = 'Cambridge';
 }
 
-const List<String> locations = <String>['Cambridge', 'Manchester', 'Oxford'];
+// const List<String> locations = <String>['Cambridge', 'Manchester', 'Oxford'];
+const List<String> locations = <String>[
+  'Cambridge',
+  'Manchester',
+  'Oxford',
+  'London',
+  'New York',
+  'Orlando',
+  'Paris',
+  'Rome',
+  'San Francisco',
+  'Tokyo',
+  'Toronto',
+  'Vancouver',
+  'Venice',
+  'Vienna',
+  'Washington DC'
+];
--- lib/weather_api_client.dart
+++ lib/weather_api_client.dart
@@ -3,11 +3,34 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_application_1/weather_model.dart';
 
-class WeatherApiClient{
+// hard coded geolocations
+const Map<String, List<double>> hardCodedLocations = {
+  'Cambridge': [52.2053, 0.1218],
+  'Manchester': [53.4808, -2.2426],
+  'Oxford': [51.7520, -1.2577],
+  'London': [51.5085, -0.1257],
+  'New York': [40.7128, -74.0060],
+  'Orlando': [28.5383, -81.3792],
+  'Paris': [48.8566, 2.3522],
+  'Rome': [41.9028, 12.4964],
+  'San Francisco': [37.7749, -122.4194],
+  'Tokyo': [35.6762, 139.6503],
+  'Toronto': [43.6532, -79.3832],
+  'Vancouver': [49.2827, -123.1207],
+  'Venice': [45.4408, 12.3155],
+  'Vienna': [48.2082, 16.3738],
+  'Washington DC': [38.9072, -77.0369]
+};
 
+class WeatherApiClient {
   // Get latitude and longitude of location
   Future<List>? getCoordinates(String? location) async {
-    var endpoint = Uri.parse('https://api.openweathermap.org/geo/1.0/direct?q=$location&appid=01e382c94f9a635a208f2368295c3597');
+    if (hardCodedLocations.containsKey(location)) {
+      return Future.value(hardCodedLocations![location]);
+    }
+
+    var endpoint = Uri.parse(
+        'https://api.openweathermap.org/geo/1.0/direct?q=$location&appid=01e382c94f9a635a208f2368295c3597');
     var response = await http.get(endpoint);
     var body = jsonDecode(response.body);
     return [body[0]['lat'], body[0]['lon']];
@@ -18,12 +41,11 @@ class WeatherApiClient{
     List? latLon = await getCoordinates(location);
     double? lat = latLon?[0];
     double? lon = latLon?[1];
-    var endpoint = Uri.parse('https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$lon&exclude=minutely,daily,alerts&units=metric&appid=01e382c94f9a635a208f2368295c3597');
+    var endpoint = Uri.parse(
+        'https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$lon&exclude=minutely,daily,alerts&units=metric&appid=01e382c94f9a635a208f2368295c3597');
     var response = await http.get(endpoint);
     var body = jsonDecode(response.body);
 
     return Weather.fromJson(body);
   }
-
-
 }
